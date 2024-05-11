1a2e0a3ecb80b5cefec3936708e3f9998305a1f0
yunsi-goh <144868418+yunsi-goh@users.noreply.github.com>
2024-05-11 14:58:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,19 @@ def index():
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    # frontend q, backend r (wrong naming convention)
+    # frontend q, backend r (wrong naming convention)s
     # Refer to pypi chapter 8 for the right convention
     r = request.form.get("q")
     return(render_template("main.html",r=r))
 
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    return(render_template("prediction.html"))
+    
+@app.route("/dbs_price",methods=["GET","POST"])
+def dbs_price():
+    q = float(request.form.get("q")) # convert to float, use 
+    return(render_template("dbs_price.html", r=(q*-50.601)+90.2))
+    
 if __name__ == "__main__":
     app.run()
